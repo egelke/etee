33d13c3d13160e1db337a52ba1945104c148645f
Bryan Brouckaert <info@egelke.net>
2013-03-28 14:51:40 +0000

    Added CIVICS test via config file to Chapter IV test

---
+++ chapterIV-test/CIVICSExample.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Egelke.EHealth.Client.ChapterIV;
+using System.ServiceModel;
+using System.Xml;
+
+namespace Egelke.EHealth.Client.ChapterIVTest
+{
+    [TestClass]
+    public class CIVICSExample
+    {
+        [TestMethod]
+        public void ConfigViaFile()
+        {
+            SamcivicsPortTypeClient client = new SamcivicsPortTypeClient("CivicsForDoctor");
+
+            ParagraphRequestType request = new ParagraphRequestType();
+            request.language = LanguageType.nl;
+            request.chapterName = "IV";
+            request.paragraphName = "30200";
+
+
+            try
+            {
+                GetParagraphIncludedSpecialitiesResponseType response = client.getParagraphIncludedSpecialities(request);
+            }
+            catch (FaultException<BusinessError> e)
+            {
+                XmlNode[] detailNodes = e.Detail.Nodes;
+                XmlNode code = detailNodes.Where(n => n.Name == "Code").Single();
+                XmlNode message = detailNodes.Where(n => n.Name == "Message").Single();
+                Assert.Fail(code.InnerText + ": " + message.InnerText);
+            }
+
+        }
+    }
+}
