869d117badcbbc5c67bb10c8c7a73192b208ab49
a434463 <bryan.brouckaert.ext@atos.net>
2021-07-05 14:32:11 +0200

    Cleanup of the lib-core tests.

--- library-core-tests/IntegrationTest.cs
+++ library-core-tests/IntegrationTest.cs
@@ -1,19 +1,16 @@
 using Egelke.EHealth.Client.Pki;
 using Egelke.EHealth.Client.Sso.Sts;
-using Egelke.Wcf.Client;
+using Egelke.Eid.Client;
+using Egelke.Wcf.Client.Helper;
 using Egelke.Wcf.Client.Security;
 using Egelke.Wcf.Client.Sts.Saml11;
-using Microsoft.IdentityModel.Protocols.WsFed;
-using Microsoft.IdentityModel.Protocols.WsTrust;
 using System;
 using System.Collections.Generic;
-using System.IdentityModel.Selectors;
-using System.IdentityModel.Tokens;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
-using System.ServiceModel.Federation;
 using System.ServiceModel.Security;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -22,10 +19,63 @@ using Xunit;
 
 namespace library_core_tests
 {
+    public class MyX509Certificate2 : X509Certificate2
+    {
+        public MyX509Certificate2(X509Certificate2 cert) : base(cert)
+        {
+
+        }
+
+        public MyX509Certificate2(String file, String pwd) : base(file, pwd)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return this.GetNameInfo(X509NameType.SimpleName, false);
+        }
+    }
+
     public class IntegrationTest
     {
-        [Fact]
-        public void Soap11Wss10()
+        public static IEnumerable<object[]> GetCerts()
+        {
+            return certs;
+        }
+
+
+        private static List<object[]> certs;
+
+        static IntegrationTest()
+        {
+            using (var readers = new Readers(ReaderScope.User))
+            using (var store = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                store.Open(OpenFlags.ReadOnly);
+                certs = readers.ListCards()
+                    .OfType<EidCard>()
+                    .Select(c =>
+                    {
+                        c.Open();
+                        String thumbprint = c.AuthCert.Thumbprint;
+                        c.Close();
+                        return store.Certificates.Find(X509FindType.FindByThumbprint, thumbprint, false)[0];
+                    })
+                    .Select(c => new object[] { new MyX509Certificate2(c) })
+                    .ToList();
+            }
+            certs.Add(new object[] { new MyX509Certificate2("eccert.p12", "Test_001") });
+        }
+
+        public IntegrationTest()
+        {
+            ECDSAConfig.Init(); //needed to enable ECDSA globally.
+        }
+
+        [Theory]
+        [MemberData(nameof(GetCerts))]
+        public void Soap11Wss10(X509Certificate2 cert)
         {
             var binding = new CustomBinding();
             binding.Elements.Add(new CustomSecurityBindingElement()
@@ -46,10 +96,7 @@ namespace library_core_tests
             //var ep = new EndpointAddress("https://localhost:44373/Echo/service.svc/soap11wss10");
             var ep = new EndpointAddress("https://localhost:8080/services/Echo");
             ChannelFactory<IEchoService> channelFactory = new ChannelFactory<IEchoService>(binding, ep);
-            if (Config.Instance.Thumbprint != null)
-                channelFactory.Credentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByThumbprint, Config.Instance.Thumbprint);
-            else
-                channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
+            channelFactory.Credentials.ClientCertificate.Certificate = cert;
 
             IEchoService client = channelFactory.CreateChannel();
 
@@ -57,8 +104,9 @@ namespace library_core_tests
             Assert.Equal("boe", pong);
         }
 
-        [Fact]
-        public void Soap12Wss10()
+        [Theory]
+        [MemberData(nameof(GetCerts))]
+        public void Soap12Wss10(X509Certificate2 cert)
         {
             var binding = new CustomBinding();
             binding.Elements.Add(new CustomSecurityBindingElement()
@@ -79,10 +127,7 @@ namespace library_core_tests
             //var ep = new EndpointAddress("https://localhost:44373/Echo/service.svc/soap11wss10");
             var ep = new EndpointAddress("https://localhost:8080/services/Echo12");
             ChannelFactory<IEchoService> channelFactory = new ChannelFactory<IEchoService>(binding, ep);
-            if (Config.Instance.Thumbprint != null)
-                channelFactory.Credentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByThumbprint, Config.Instance.Thumbprint);
-            else
-                channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
+            channelFactory.Credentials.ClientCertificate.Certificate = cert;
 
             IEchoService client = channelFactory.CreateChannel();
 
@@ -90,15 +135,10 @@ namespace library_core_tests
             Assert.Equal("boe", pong);
         }
 
-        [Fact]
-        public void Soap12Wss11()
+        [Theory]
+        [MemberData(nameof(GetCerts))]
+        public void Soap12Wss11(X509Certificate2 cert)
         {
-            /*
-            var binding = new WSHttpBinding(SecurityMode.TransportWithMessageCredential, false);
-            binding.Security.Message.EstablishSecurityContext = false;
-            binding.Security.Message.ClientCredentialType = MessageCredentialType.Certificate;
-            */
-
             var binding = new CustomBinding();
             binding.Elements.Add(new CustomSecurityBindingElement()
             {
@@ -119,10 +159,7 @@ namespace library_core_tests
             //var ep = new EndpointAddress("https://localhost:44373/Echo/service.svc/soap11wss10");
             var ep = new EndpointAddress("https://localhost:8080/services/Echo12");
             ChannelFactory<IEchoService> channelFactory = new ChannelFactory<IEchoService>(binding, ep);
-            if (Config.Instance.Thumbprint != null)
-                channelFactory.Credentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByThumbprint, Config.Instance.Thumbprint);
-            else
-                channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
+            channelFactory.Credentials.ClientCertificate.Certificate = cert;
 
             IEchoService client = channelFactory.CreateChannel();
 
@@ -130,8 +167,9 @@ namespace library_core_tests
             Assert.Equal("boe", pong);
         }
 
-        [Fact]
-        public void Soap11Wss10SignAll()
+        [Theory]
+        [MemberData(nameof(GetCerts))]
+        public void Soap11Wss10SignAll(X509Certificate2 cert)
         {
             var binding = new CustomBinding();
             binding.Elements.Add(new CustomSecurityBindingElement()
@@ -153,10 +191,7 @@ namespace library_core_tests
             //var ep = new EndpointAddress("https://localhost:44373/Echo/service.svc/soap11wss10");
             var ep = new EndpointAddress("https://localhost:8080/services/Echo");
             ChannelFactory<IEchoService> channelFactory = new ChannelFactory<IEchoService>(binding, ep);
-            if (Config.Instance.Thumbprint != null)
-                channelFactory.Credentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByThumbprint, Config.Instance.Thumbprint);
-            else
-                channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
+            channelFactory.Credentials.ClientCertificate.Certificate = cert;
 
             IEchoService client = channelFactory.CreateChannel();
 
@@ -170,9 +205,9 @@ namespace library_core_tests
         public void EhealthStsSaml11()
         {
             var doc = new XmlDocument();
-            X509Certificate2 cert = Config.Instance.Certificate;
+            X509Certificate2 cert = (X509Certificate2) certs.FirstOrDefault()[0];
             Match match = Regex.Match(cert.Subject, @"SERIALNUMBER=(\d{11}),");
-            if (!match.Success) throw new InvalidProgramException("need an ssin in the cert subject");
+            Assert.True(match.Success, "need an ssin in the cert subject (is an eID available?)");
             string ssin = match.Groups[1].Value;
 
             var claims = new List<XmlElement>();
@@ -196,8 +231,8 @@ namespace library_core_tests
             attrValText.Data = ssin;
             claims.Add(attr);
 
-            var session = new EHealthP12("ehealth-01050399864-int.p12", File.ReadAllText("ehealth-01050399864-int.pwd"));
-            //var session = new EHealthP12("ehealth-79021802145-acc.p12", File.ReadAllText("ehealth-79021802145-acc.pwd"));
+            var session = new EHealthP12("ehealth-01050399864-int.p12", File.ReadAllText("ehealth-01050399864-int.p12.pwd"));
+            //var session = new EHealthP12("ehealth-79021802145-acc.p12", File.ReadAllText("ehealth-79021802145-acc.p12.pwd"));
             var binding = new StsBinding()
             {
                 //BypassProxyOnLocal = false,
@@ -208,84 +243,13 @@ namespace library_core_tests
             var ep = new EndpointAddress("https://services-int.ehealth.fgov.be/IAM/Saml11TokenService/v1");
             //var ep = new EndpointAddress("https://services-acpt.ehealth.fgov.be/IAM/Saml11TokenService/v1");
             StsClient target = new StsClient(binding, ep);
-            if (Config.Instance.Thumbprint != null)
-                target.ClientCredentials.ClientCertificate.SetCertificate(StoreLocation.CurrentUser, StoreName.My, X509FindType.FindByThumbprint, Config.Instance.Thumbprint);
-            else
-                target.ClientCredentials.ClientCertificate.Certificate = Config.Instance.Certificate;
+            target.ClientCredentials.ClientCertificate.Certificate = cert;
             XmlElement assertion = target.RequestTicket("Anonymous", session["authentication"], TimeSpan.FromHours(1), claims, claims);
-        }
 
-        [Fact]
-        public void EhealthStsWsTrust()
-        {
-            var issuerBinding = new StsBinding();
-            issuerBinding.BypassProxyOnLocal = false;
-            issuerBinding.UseDefaultWebProxy = false;
-            issuerBinding.ProxyAddress = new Uri("http://localhost:8866");
-
-            var endpointAddress = new EndpointAddress("https://services-acpt.ehealth.fgov.be/IAM/SecurityTokenService/v1");
-
-            using (var memStream = new MemoryStream())
-            {
-                var request = new WsTrustRequest(WsTrustActions.Trust13.Issue)
-                {
-                    TokenType = "http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV1.1",
-                    Claims = new Claims("http://schemas.xmlsoap.org/ws/2006/12/authorization/authclaims", new List<ClaimType>
-                    {
-                        new ClaimType()
-                        {
-                            Uri = "urn:be:fgov:person:ssin",
-                            Value = "79021802145"
-                        }
-                    })
-                };
-                var serializer = new WsTrustSerializer();
-
-                using (var writer = XmlDictionaryWriter.CreateTextWriter(memStream, Encoding.UTF8, false))
-                {
-                    serializer.WriteRequest(writer, WsTrustVersion.Trust13, request);
-                }
-
-                memStream.Position = 0;
-                var reader = XmlDictionaryReader.CreateTextReader(memStream, XmlDictionaryReaderQuotas.Max);
-
-                var requestMessage = Message.CreateMessage(MessageVersion.Soap11, WsTrustActions.Trust13.IssueRequest, reader);
-
-                ChannelFactory<IRequestChannel> channelFactory = new ChannelFactory<IRequestChannel>(issuerBinding, endpointAddress);
-                channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
-                IRequestChannel channel = channelFactory.CreateChannel();
-
-                Message reply = channel.Request(requestMessage);
-            }
-        }
-
-            [Fact]
-        public void EhealthGenIns()
-        {
-
-            var issuerBinding = new StsBinding();
-            //issuerBinding.Security.Message.ClientCredentialType = BasicHttpMessageCredentialType.Certificate;
-            //issuerBinding.Security.Message.EstablishSecurityContext = false;
-            issuerBinding.BypassProxyOnLocal = false;
-            issuerBinding.UseDefaultWebProxy = false;
-            issuerBinding.ProxyAddress = new Uri("http://localhost:8866");
-
-            var endpointAddress = new EndpointAddress("https://services-acpt.ehealth.fgov.be/IAM/SecurityTokenService/v1");
-
-            /*
-            var tokenParameters = WSTrustTokenParameters.CreateWSFederationTokenParameters(issuerBinding, endpointAddress);
-
-            
-            var binding = new System.ServiceModel.Federation.WSFederationHttpBinding(tokenParameters);
-
-            var ep = new EndpointAddress("https://services-acpt.ehealth.fgov.be/GenericInsurability/v1");
-            ChannelFactory<IEchoService> channelFactory = new ChannelFactory<IEchoService>(binding, ep);
-            channelFactory.Credentials.ClientCertificate.Certificate = Config.Instance.Certificate;
-            IEchoService client = channelFactory.CreateChannel();
-
-            String pong = client.Echo("boe");
-            Assert.Equal("boe", pong);
-            */
+            XmlNamespaceManager nsMngr = new XmlNamespaceManager(assertion.OwnerDocument.NameTable);
+            nsMngr.AddNamespace("s11", "urn:oasis:names:tc:SAML:1.0:assertion");
+            Assert.Equal("urn:be:fgov:ehealth:sts:1_0", assertion.SelectSingleNode("@Issuer").Value);
+            Assert.Equal(ssin, assertion.SelectSingleNode("./s11:AttributeStatement/s11:Attribute[@AttributeName='urn:be:fgov:person:ssin']/s11:AttributeValue/text()", nsMngr).Value);
         }
     }
 
